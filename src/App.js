019e3e8c7463774b5ee4f168f8f4eee7139966ee
Misaki <zh357964059@vip.qq.com>
2019-12-09 20:31:02 +0800

    基本文件夹和引入antd

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from 'react'
+import { Button, message } from 'antd'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/**
+ * 根组件
+ */
+export default class App extends Component {
+
+
+    handleClick = () => {
+        message.success('success!')
+    }
+
+    render(){
+        return (
+            <div>
+                <Button type="primary" onClick={this.handleClick}>Primary</Button>
+            </div>
+        )
+    }
 }
-
-export default App;
