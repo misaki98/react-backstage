7a522e6188852b4cbd7a0fee0dd0b0ebaede04ea
Misaki <zh357964059@vip.qq.com>
2019-12-10 16:22:01 +0800

    登录组件和表单验证

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
-import React,{Component} from 'react'
-import { Button, message } from 'antd'
+import React, { Component } from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
+import Login from './pages/login/login'
+import Admin from './pages/admin/admin'
 /**
  * 根组件
  */
 export default class App extends Component {
 
 
-    handleClick = () => {
-        message.success('success!')
-    }
 
-    render(){
+
+    render() {
         return (
-            <div>
-                <Button type="primary" onClick={this.handleClick}>Primary</Button>
-            </div>
+            // 最外层都是一个路由器
+            <BrowserRouter>
+                <Switch>
+                    {/* 在路由器内部配置路由，每个路由都是一个标签,Switch只匹配其中一个 */}
+                    <Route path='/login' component={Login}></Route>
+                    <Route path='/' component={Admin}></Route>
+                </Switch>
+            </BrowserRouter>
         )
     }
 }
---
+++ src/pages/admin/admin.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+/**
+ * 管理页面的路由组件
+ */
+export default class Admin extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {}
+    }
+
+    render() {
+        return (
+            null
+        )
+    }
+}
