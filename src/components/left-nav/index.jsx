8b348df0e1e448c7e80fe9cb9462a8ecf63cc533
Misaki <zh357964059@vip.qq.com>
2019-12-15 13:54:09 +0800

    bug fixed

--- src/components/left-nav/index.jsx
+++ src/components/left-nav/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, withRouter } from 'react-router-dom'
 import { Menu, Icon } from 'antd'
 
 import logo from '../../assets/images/logo.png'
@@ -10,11 +10,11 @@ const { SubMenu } = Menu
 /**
  * 左侧导航组件
  */
-export default class LeftNav extends React.Component {
+class LeftNav extends React.Component {
     constructor(props) {
         super(props)
         this.state = {}
-    }
+    } 
 
     getMenuNodes = (menuList) => {
         // 根据传入的menu数据数组生产对应的标签数组
@@ -29,6 +29,8 @@ export default class LeftNav extends React.Component {
                     </Menu.Item>
                 )
             } else {
+                const path = this.props.location.pathname
+                if (item.children.find(cItem => path.indexOf(cItem.key) === 0)) { this.targetNode = item.key }
                 return (
                     <SubMenu
                         key={item.key}
@@ -45,9 +47,26 @@ export default class LeftNav extends React.Component {
             }
         })
     }
+    componentWillMount() {
+        this.menuNodes = this.getMenuNodes(menuList)
+    }
 
-
+    // 一个根据地址动态进行列表展开的函数
+    // pathOpen = (path) =>{
+    //     const target = path.split('/')[1]
+    //     if(target==='category' || target === 'product'){
+    //         return '/products'
+    //     }else if(target === 'charts'){
+    //         return '/charts'
+    //     }else{
+    //         return ''
+    //     }
+    // }
     render() {
+        // 得到当前请求的路由路径
+        const path = this.props.location.pathname
+        const target = this.targetNode
+
         return (
             <div className="left-nav">
                 <Link className="left-nav-header" to='/'>
@@ -55,85 +74,12 @@ export default class LeftNav extends React.Component {
                     <h1>硅谷后台</h1>
                 </Link>
                 <Menu
-                    defaultSelectedKeys={['1']}
-                    defaultOpenKeys={['sub1']}
+                    selectedKeys={[path]}
+                    defaultOpenKeys={[target]}
                     mode="inline"
                     theme="dark"
                 >
-                    {this.getMenuNodes(menuList)}
-                    {/* <Menu.Item key="home">
-                        <Link to='/home'>
-                            <Icon type="home" />
-                            <span>首页</span>
-                        </Link>
-                    </Menu.Item>
-                    <SubMenu
-                        key="sub2"
-                        title={
-                            <span>
-                                <Icon type="appstore" />
-                                <span>商品</span>
-                            </span>
-                        }
-                    >
-                        <Menu.Item key="category">
-                            <Link to='/category'>
-                                <Icon type="menu" />
-                                <span>品类管理</span>
-                            </Link>
-
-                        </Menu.Item>
-                        <Menu.Item key="product">
-                            <Link to='/product'>
-                                <Icon type="tool" />
-                                <span>商品管理</span>
-                            </Link>
-
-                        </Menu.Item>
-                    </SubMenu>
-                    <Menu.Item key="user">
-                        <Link to='/user'>
-                            <Icon type="user" />
-                            <span>用户管理</span>
-                        </Link>
-                    </Menu.Item>
-                    <Menu.Item key="role">
-                        <Link to='/role'>
-                            <Icon type="usergroup-add" />
-                            <span>角色管理</span>
-                        </Link>
-                    </Menu.Item>
-                    <SubMenu
-                        key="sub3"
-                        title={
-                            <span>
-                                <Icon type="area-chart" />
-                                <span>图表</span>
-                            </span>
-                        }
-                    >
-                        <Menu.Item key="bar">
-                            <Link to='/charts/bar'>
-                                <Icon type="bar-chart" />
-                                <span>柱状图</span>
-                            </Link>
-
-                        </Menu.Item>
-                        <Menu.Item key="line">
-                            <Link to='/charts/line'>
-                                <Icon type="line-chart" />
-                                <span>折线图</span>
-                            </Link>
-
-                        </Menu.Item>
-                        <Menu.Item key="pie">
-                            <Link to='/charts/pie'>
-                                <Icon type="pie-chart" />
-                                <span>饼图</span>
-                            </Link>
-
-                        </Menu.Item>
-                    </SubMenu> */}
+                    {this.menuNodes}
                 </Menu>
             </div>
 
@@ -141,3 +87,8 @@ export default class LeftNav extends React.Component {
         )
     }
 }
+
+/**
+ * 高阶组件 包装非路由组件，向非路由组件传递3个属性，history，location，match
+ */
+export default withRouter(LeftNav)
